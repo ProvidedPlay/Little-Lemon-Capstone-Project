e22ee3a22f573b3cc08c969e2b2aa771111cc2e9
ProvidedPlay <mosesreid@hotmail.com>
2024-06-21 20:06:49 -0400

    made the whole page reactive, incl text and elements

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,10 @@ import PageSection from "./PageSection";
 const Main = ({children}) => {
 
     return(
-        <div className="Main">
-        {children.map((child) => <PageSection children={child}/>)}
+        <div className="window">
+            <div className="webpage">
+                {children.map((child) => <PageSection children={child}/>)}
+            </div>
         </div>
     )
 }
